48769789502d34bd7fb27a05b78a26f44c0896ca
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-08-12 17:01:44 -0600

    Corrección del footer, por el momento me pareció innecesario mostrar las redes sociales que no tienen que ver con lo profesional que ya se muestran en el hero

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,68 +1,7 @@
 export default function Footer() {
   return (
-    <footer id="bottom-footer" className="bg-white shadow-lg mt-10 py-5">
-      <div className="container flex gap-5 justify-between items-center">
-        <p className="opacity-80">Jorge Armando Ceras Cárdenas</p>
-
-        <ul className="flex flex-wrap gap-2">
-          <li>
-            <a
-              href="https://x.com/armandodev_/"
-              title="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="icon hover:scale-125 transition-transform duration-200 ease-in-out"
-                src="/icons/twitter.svg"
-                alt="Twitter"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.github.com/armandodev"
-              title="GitHub"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="icon hover:scale-125 transition-transform duration-200 ease-in-out"
-                src="/icons/github.svg"
-                alt="GitHub"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              title="LinkedIn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="icon hover:scale-125 transition-transform duration-200 ease-in-out"
-                src="/icons/linkedin.svg"
-                alt="LinkedIn"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="mailto:jorge.armando.c.cardenas@gmail.com"
-              title="Email"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="icon hover:scale-125 transition-transform duration-200 ease-in-out"
-                src="/icons/gmail.svg"
-                alt="Email"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+    <footer className="bg-white border-t border-gray-300 mt-10 py-5">
+      <p className="opacity-80 text-center">Jorge Armando Ceras Cárdenas</p>
     </footer>
   );
 }
