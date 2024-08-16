a22c010235cc00f3cac80565ac560ca94452cbd7
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-08-15 20:55:10 -0600

    Se creo el componente Skill para la sección de Skills eliminando la acción repetitiva del agregar habilidades, además se colocaron en una lista para iterar que en un futuro con el backend podremos recuperar esta información de una base de datos ya que el frontend estará preparado.

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,56 +1,30 @@
 import Article from "../components/Article";
+import Skill from "../components/Skill";
 
 export default function Skills() {
+  const skills = [
+    { id: 1, icon: "/icons/open-book.svg", name: "Inglés" },
+    { id: 2, icon: "/icons/html.svg", name: "HTML" },
+    { id: 3, icon: "/icons/css.svg", name: "CSS" },
+    { id: 4, icon: "/icons/javascript.svg", name: "JavaScript" },
+    { id: 5, icon: "/icons/react.svg", name: "React" },
+    { id: 6, icon: "/icons/tailwind.svg", name: "TailwindCSS" },
+  ];
+
   return (
     <Article articleId="skills" className="min-h-fit">
       <section className="w-full grid gap-5">
         <h2 className="text-3xl font-bold">{">"} Habilidades</h2>
 
         <ul className="grid grid-cols-3 gap-8 place-items-center sm:grid-cols-5 md:grid-cols-6">
-          <li className="w-full aspect-square grid gap-2 place-items-center p-2">
-            <img
-              className="w-[80%] aspect-square object-cover object-center sm:w-full"
-              src="/icons/html.svg"
-              alt="HTML"
-            />
-            <span className="text-xs font-medium opacity-80">HTML</span>
-          </li>
-          <li className="w-full aspect-square grid gap-2 place-items-center p-2">
-            <img
-              className="w-[80%] aspect-square object-cover object-center sm:w-full"
-              src="/icons/css.svg"
-              alt="CSS"
-            />
-            <span className="text-xs font-medium opacity-80">CSS</span>
-          </li>
-          <li className="w-full aspect-square grid gap-2 place-items-center p-2">
-            <img
-              className="w-[80%] aspect-square object-cover object-center sm:w-full"
-              src="/icons/javascript.svg"
-              alt="JavaScript"
-            />
-            <span className="text-xs font-medium opacity-80">JavaScript</span>
-          </li>
-          <li className="w-full aspect-square grid gap-2 place-items-center p-2">
-            <img
-              className="w-[80%] aspect-square object-cover object-center sm:w-full"
-              src="/icons/react.svg"
-              alt="React"
-            />
-            <span className="text-xs font-medium opacity-80">React</span>
-          </li>
-          <li className="w-full aspect-square grid gap-2 place-items-center p-2">
-            <img
-              className="w-[80%] aspect-square object-cover object-center sm:w-full"
-              src="/icons/tailwind.svg"
-              alt="TailwindCSS"
-            />
-            <span className="text-xs font-medium opacity-80">TailwindCSS</span>
-          </li>
+          {skills.map((skill) => (
+            <Skill skill={skill} key={skill.id} />
+          ))}
         </ul>
 
         <p className="text-sm font-light opacity-80 text-center">
-          Estoy abierto a aprender nuevas tecnologías y herramientas.
+          Estoy abierto a aprender nuevas tecnologías, herramientas y
+          habilidades.
         </p>
       </section>
     </Article>
