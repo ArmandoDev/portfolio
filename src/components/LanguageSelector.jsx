ac472b1cae47a8858b825c9a1cde91d279293837
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-08-15 20:51:51 -0600

    Esta eliminación es parte de la eliminación anterior, de lo mismo descartado de la internacionalización

--- src/components/LanguageSelector.jsx
+++
@@ -1,9 +0,0 @@
-export default function LanguageSelector() {
-  return (
-    <li className="flex gap-1 items-center" title="Change language">
-      <img className="icon" src="/icons/us-flag.svg" alt="English" />
-      EN
-    </li>
-  );
-}
-/* Lógica de i18n pendiente */
