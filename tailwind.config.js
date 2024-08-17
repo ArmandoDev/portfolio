6c47a886e8ea212c4a1889596bd6e732878b68eb
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-08-16 18:11:44 -0600

    Se ha agregado la librería de animaciones hecha por midudev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animations],
 };
